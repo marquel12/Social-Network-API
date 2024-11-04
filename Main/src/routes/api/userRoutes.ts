73f59f43b6910a135f7b61e6e9469ea882e5ae04
Marquel <marqueljamal12@gmail.com>
2024-11-04 00:26:11 -0500

    updated user routes

---
+++ Main/src/routes/api/userRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+const router = Router();
+import { getUsers, 
+    getSingleUser, 
+    createUser, 
+    updateUserById, 
+    deleteUserById, 
+    addNewFriend, 
+    deleteFriend, } 
+    from '../../controllers/videoController.js';
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUserById)
+  .delete(deleteUserById);
+
+
+
+// /api/users:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addNewFriend).delete(deleteFriend);
+
+export default router;
